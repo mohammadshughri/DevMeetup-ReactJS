fcd165805fa5a69ef0c2fc656dfcdb7234a15140
MohammadShughri <Mohammadws19@gmail.com>
2021-07-05 13:16:01 +0300

    Added componentes to all meetups page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch } from "react-router-dom";
 
-import AllMeetups from './pages/AllMeetups';
-import Favorites from './pages/Favorites';
-import NewMeetup from './pages/NewMeetup';
-import MainNavigation from './components/layout/MainNavigation';
+import AllMeetups from "./pages/AllMeetups";
+import Favorites from "./pages/Favorites";
+import NewMeetup from "./pages/NewMeetup";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
-  return <div>
-    <MainNavigation />
-    <Switch>
-      <Route path='/' exact>
-       <AllMeetups />
-     </Route>
-      <Route path='/favorites'>
-        <Favorites/>
-      </Route>
-      <Route path='/newmeetup'>
-        <NewMeetup/>
-      </Route>
-    </Switch>
-  </div>;
-
+  return (
+    <div>
+      <MainNavigation />
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetups />
+        </Route>
+        <Route path="/favorites">
+          <Favorites />
+        </Route>
+        <Route path="/newmeetup">
+          <NewMeetup />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,24 +1,26 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-import classes from './MainNavigation.module.css';
+import classes from "./MainNavigation.module.css";
 
-function MainNavigation () {
-    return <header className={classes.header}>
-        <div className={classes.logo}>React Meetups</div>
-        <nav>
-            <ul>
-                <li>
-                    <Link to='/'>All Meetups</Link>
-                </li>
-                <li>
-                    <Link to='/newmeetup'>Add New Meetup</Link>
-                </li>
-                <li>
-                    <Link to='/favorites'>My Favorites</Link>
-                </li>
-            </ul>
-        </nav>
+function MainNavigation() {
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>React Meetups</div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">All Meetups</Link>
+          </li>
+          <li>
+            <Link to="/newmeetup">Add New Meetup</Link>
+          </li>
+          <li>
+            <Link to="/favorites">My Favorites</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
+  );
 }
 
 export default MainNavigation;
