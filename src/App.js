459a45291e32ad27acd59a4b5e3ad1046350eb40
MohammadShughri <Mohammadws19@gmail.com>
2021-07-05 14:39:38 +0300

    Added cards

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetups from "./pages/AllMeetups";
 import Favorites from "./pages/Favorites";
 import NewMeetup from "./pages/NewMeetup";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Switch>
         <Route path="/" exact>
           <AllMeetups />
@@ -20,7 +19,7 @@ function App() {
           <NewMeetup />
         </Route>
       </Switch>
-    </div>
+    </Layout>
   );
 }
 
