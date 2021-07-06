abe9a90e7b074534c7299aeaf43fc00e69f6cc50
MohammadShughri <Mohammadws19@gmail.com>
2021-07-06 10:09:49 +0300

    Added firebase support

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -3,7 +3,7 @@ import { useRef } from "react";
 import Card from "../ui/Card";
 import classes from "./NewMeetupForm.module.css";
 
-function NewMeetupForm() {
+function NewMeetupForm(props) {
   const titleInputRef = useRef();
   const imageInputRef = useRef();
   const addressInputRef = useRef();
@@ -17,13 +17,16 @@ function NewMeetupForm() {
     const enteredAddress = addressInputRef.current.value;
     const enteredDescription = descriptionInputRef.current.value;
 
+    //Store the entered values as values
     const meetupData = {
       title: enteredTitle,
       image: enteredImage,
       address: enteredAddress,
       description: enteredDescription,
     };
+    props.onAddMeetup(meetupData);
 
+    //Show the entered data in the console
     console.log(meetupData);
   }
   return (
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,30 @@
+import { useHistory } from "react-router-dom";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetup() {
+  const history = useHistory();
+
+  function addMeetupHandler(meetupDate) {
+    //connecting firebase database with the local database
+    fetch(
+      "https://reactjs-meetup-3c8ed-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupDate),
+        //headers is used to identify that this request carries JSON data
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+      //Navigating away to the main page once the form is filled
+    ).then(() => {
+      history.replace("/");
+    });
+  }
   return (
     <section>
       <h1>New Meetup Page</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
