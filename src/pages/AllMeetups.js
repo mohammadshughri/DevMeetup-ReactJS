04c717156066367bc913206c32657a59bd1f9c4e
MohammadShughri <Mohammadws19@gmail.com>
2021-07-06 10:53:05 +0300

    Added useEffect

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,29 +1,48 @@
+import { useState, useEffect } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
-const DUMMY_DATA = [
-  {
-    id: "m1",
-    title: "First meetup: Silicon Valley",
-    image:
-      "https://blog.youthall.com/wp-content/uploads/2020/11/silicon-780x450.png",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is the first meetup, located in silicon valley, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-  {
-    id: "m2",
-    title: "Second meetup: Tokyo",
-    image: "https://www.hisglobal.com.tr/assets/images/uploads/1583759608.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description: "This is the second meetup. It will be located in Tokyo",
-  },
-];
-
 function AllMeetups() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    fetch(
+      "https://reactjs-meetup-3c8ed-default-rtdb.firebaseio.com/meetups.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((date) => {
+        const meetups = [];
+
+        for (const key in date) {
+          const meetup = {
+            id: key,
+            ...date[key],
+          };
+
+          meetups.push(meetup);
+        }
+
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
   return (
     <section>
       <h1>All Meetups</h1>
-      <MeetupList meetups={DUMMY_DATA} />
+      <MeetupList meetups={loadedMeetups} />
     </section>
   );
 }
